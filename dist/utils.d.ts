59c8a3ad62da349d16155218f43a7a50ece92f4f
Mr. Best <kosal_soem@outlook.com>
2025-05-07 00:02:01 +0700

    fix bugs and add ssl checker function

--- dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const defaultCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-export declare function generateRandomString(length: number, charset: string): string;
-export declare function computeChecksum(code: string): string;
